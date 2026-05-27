cb9c9f06bbef74f9837f630ff10e7b30bf699d91
OrdinalDragon <ordinaldragon@gmail.com>
2026-05-27 16:39:58 -0300

    Empezando con DOM y BOM

---
+++ clase-01/02.dom-bom/src/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+
+console.warn('// ! dom -> document object model');
+
+console.log(document);
+
+console.warn('// ! BOM -> Browser Object model');
+
+console.log(window);
+
+// ECMAScript (Genera los estandares para que Javascript se pueda utilizar en todos los navegadores y el backend de la misma manera)
+
+// https://ecma-international.org/
